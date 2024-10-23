00dce06166d5ce879c17b9238925533d051cdfe3
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-10-23 18:13:37 +0530

    btn for resume

---
+++ app/components/Button.tsx
@@ -0,0 +1,172 @@
+import "../globals.css";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
+
+interface GlowButtonProps {
+  children: React.ReactNode;
+  color?: string;
+  onClick?: () => void;
+}
+
+export const GlowButton: React.FC<GlowButtonProps> = ({
+  children,
+  color = "#14013d",
+  onClick,
+  ...props
+}) => {
+  const buttonRef = useRef<HTMLDivElement | null>(null);
+  const [buttonWidth, setButtonWidth] = useState<number>(0);
+  const [buttonHeight, setButtonHeight] = useState<number>(0);
+  const [mousePosition, setMousePosition] = useState({ x: -240000, y: 0 });
+
+  // Use effect to set button width and height
+  useEffect(() => {
+    if (buttonRef.current) {
+      setButtonWidth(buttonRef.current.offsetWidth);
+      setButtonHeight(buttonRef.current.offsetHeight);
+    }
+  }, [buttonRef]);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const mouseX = e.pageX - e.currentTarget.offsetLeft;
+    const mouseY = e.pageY - e.currentTarget.offsetTop;
+
+    const x = (mouseX / buttonWidth) * 100;
+    const y = (mouseY / buttonHeight) * 100;
+
+    setMousePosition({ x, y });
+  };
+
+  const cols = buttonWidth / 16;
+  const rows = buttonHeight / 16;
+
+  const shadows = [];
+
+  // Determine how many shadows to create
+  for (let i = 0; i <= cols; i++) {
+    for (let j = 0; j <= rows; j++) {
+      shadows.push(
+        <Shadow
+          key={`s${i}-${j}`}
+          left={i * (100 / cols)}
+          top={j * (100 / rows)}
+          color={
+            i < cols / 2 && j < rows / 2
+              ? "#304EAE"
+              : i > cols / 2 && j < rows / 2
+              ? "#FF6D6D"
+              : i > cols / 2 && j > rows / 2
+              ? "#FFC107"
+              : i < cols / 2 && j > rows / 2
+              ? "#00B74A"
+              : "#888888"
+          }
+          mousePosition={mousePosition}
+        />
+      );
+    }
+  }
+
+  return (
+    <motion.div
+      ref={buttonRef}
+      style={{
+        height: "4rem",
+        width: "12rem",
+        backgroundColor: "#00000000",
+        fontWeight: "bold",
+        fontSize: "1.5rem",
+        fontFamily: "var(--font-porsche-bold)", // Use the custom font here
+        position: "relative",
+        padding: 0,
+        overflow: "hidden",
+        cursor: "pointer",
+        borderRadius: "1.25rem",
+        userSelect: "none",
+        color,
+        border: `2px solid ${color}`, // Set border thickness and color
+      }}
+      initial={{ boxShadow: `0 0 0rem .5px ${color}` }}
+      whileHover={{
+        boxShadow: `0 0 0rem 1px ${color}`,
+        transition: { duration: 0.5 },
+      }}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={() => setMousePosition({ x: -24, y: -1 })}
+      onClick={onClick}
+      {...props}
+    >
+      {shadows}
+      <motion.span
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          zIndex: 1,
+          backgroundColor: "#ffffff00",
+          backdropFilter: "blur(.5rem)",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "inherit",
+        }}
+      >
+        {children}
+      </motion.span>
+      <motion.span
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          display: "block",
+          zIndex: 2,
+          borderRadius: "inherit",
+        }}
+        initial={{ boxShadow: `0 0 0 0px ${color}00 inset` }}
+        whileTap={{
+          boxShadow: [`0 0 0 3px ${color}00 inset`, `0 0 0 0px ${color} inset`],
+        }}
+        transition={{ duration: 0.5 }}
+      />
+    </motion.div>
+  );
+};
+
+const Shadow = ({
+  left,
+  top,
+  color,
+  mousePosition,
+}: {
+  left: number;
+  top: number;
+  color: string;
+  mousePosition: { x: number; y: number };
+}) => {
+  return (
+    <AnimatePresence>
+      {mousePosition.x < left + 16 &&
+        mousePosition.x > left - 16 &&
+        mousePosition.y < top + 16 &&
+        mousePosition.y > top - 16 && (
+          <motion.span
+            style={{
+              position: "absolute",
+              width: 16,
+              height: 16,
+              borderRadius: "2rem",
+              boxShadow: `0 0 1rem 0.5rem ${color}`,
+              filter: "blur(1rem)",
+              x: "-50%",
+              y: "-50%",
+              left: `${left}%`,
+              top: `${top}%`,
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, transition: { delay: 0.2 } }}
+          />
+        )}
+    </AnimatePresence>
+  );
+};
--- app/home/HomePage.tsx
+++ app/home/HomePage.tsx
@@ -3,15 +3,19 @@
 import React, { useState } from "react";
 import useMousePosition from "../utils/useMousePosition";
 import { motion } from "framer-motion";
+import { GlowButton } from "../components/Button";
 
 const HomePage = () => {
   const [isHovered, setIsHovered] = useState(false);
   const { x, y } = useMousePosition();
   const size = isHovered ? 400 : 40;
 
+  // Determine if the screen size is less than the md breakpoint
+  const isMobile = window.innerWidth < 768; // Example breakpoint, adjust as needed
+
   return (
     <div className="relative w-screen h-screen">
-      <div className="text-[256px] urbanshock text-[#EEE9C7] main">
+      <div className="text-[256px]  urbanshock text-[#EEE9C7] main">
         <motion.div
           className="mask"
           animate={{
@@ -25,11 +29,27 @@ const HomePage = () => {
             onMouseLeave={() => setIsHovered(false)}
             className="w-full"
           >
-            DHANIYEL DARVESH
+            DHANIYEL {isMobile && <br />} DARVESH
           </h1>
         </motion.div>
         <div className="body">
-          <h1 className="text-center">DHANIYEL DARVESH</h1>
+          <h1 className="text-center">DHANIYEL {isMobile && <br />} DARVESH</h1>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+          }}
+        >
+          <GlowButton
+            onClick={() => console.log("Button clicked")}
+            color={"#ffffff"}
+          >
+            RESUME
+          </GlowButton>
         </div>
       </div>
     </div>
