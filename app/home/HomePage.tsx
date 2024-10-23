fceac952e7382b9d348a1c99031e2eb88211b492
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-10-23 20:36:17 +0530

    about page creat

--- app/home/HomePage.tsx
+++ app/home/HomePage.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import React, { useState } from "react";
+import { useRouter } from "next/navigation"; // For Next.js 13 and later
 import useMousePosition from "../utils/useMousePosition";
 import { motion } from "framer-motion";
 import { GlowButton } from "../components/Button";
 import { FaAngleDoubleDown } from "react-icons/fa";
 
 const HomePage = () => {
+  const router = useRouter(); // Initialize the router
   const [isHovered, setIsHovered] = useState(false);
   const { x, y } = useMousePosition();
   const size = isHovered ? 400 : 40;
-
-  // Determine if the screen size is less than the md breakpoint
-  const isMobile = window.innerWidth < 768; // Example breakpoint, adjust as needed
 
   // Function to handle the resume download
   const handleDownload = () => {
@@ -22,6 +21,11 @@ const HomePage = () => {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link); // Clean up by removing the link after the click
+  };
+
+  // Function to navigate to the About page
+  const handleNavigate = () => {
+    router.push("/about"); // Navigate to the About page
   };
 
   return (
@@ -40,11 +44,13 @@ const HomePage = () => {
             onMouseLeave={() => setIsHovered(false)}
             className="w-full"
           >
-            DHANIYEL {isMobile && <br />} DARVESH
+            DHANIYEL {window.innerWidth < 768 && <br />} DARVESH
           </h1>
         </motion.div>
         <div className="body">
-          <h1 className="text-center">DHANIYEL {isMobile && <br />} DARVESH</h1>
+          <h1 className="text-center">
+            DHANIYEL {window.innerWidth < 768 && <br />} DARVESH
+          </h1>
         </div>
         <div
           style={{
@@ -60,12 +66,13 @@ const HomePage = () => {
           </GlowButton>
           <div className="absolute bottom-5">
             <motion.div
-              className="border-[#eee9c7] border-[2px] w-10 h-10 rounded-full flex justify-center items-center "
+              className="border-[#eee9c7] border-[2px] w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
               whileHover={{
                 scale: 1.2, // Slight scaling effect on hover
                 rotate: 360, // Rotate the icon on hover
                 transition: { duration: 0.4, ease: "easeInOut" }, // Smooth transition
               }}
+              onClick={handleNavigate} // Navigate when clicked
             >
               <FaAngleDoubleDown className="w-5 h-5 text-white" />
             </motion.div>
