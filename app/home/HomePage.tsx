94dbefa2b617691625af1282424f82cd30cdd343
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-10-23 16:18:54 +0530

    mouse position

--- app/home/HomePage.tsx
+++ app/home/HomePage.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <div className=""></div>
+      <div className="text-[256px] urbanshock text-[#EEE9C7]">
+        <h1>DHANIYEL DARVESH</h1>
+      </div>
     </div>
   );
 };
