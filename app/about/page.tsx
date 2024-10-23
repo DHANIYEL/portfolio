16b0ec922676a139ba03724a49833219b8e2cc86
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-10-23 22:21:12 +0530

    about hover

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,47 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import useMousePosition from "../utils/useMousePosition";
+import { motion } from "framer-motion";
+import "./style.css";
 
 const About = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 200 : 40;
+
   return (
-    <div>
-      <div className="w-screen flex justify-start items-center p-10">
-        <h1 className="text-9xl text-txt  urbanshock">ABOUT</h1>
+    <div className="relative w-screen">
+      <div className="text-9xl urbanshock text-[#EEE9C7] about_main">
+        <motion.div
+          className="about_mask"
+          animate={{
+            WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`, // Center the mask at mouse position
+            WebkitMaskSize: `${size}px`,
+          }}
+          transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+        >
+          <h1
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            className=""
+          >
+            ABOUT
+          </h1>
+        </motion.div>
+        <div className="about_body">
+          <h1 className="text-center">ABOUT</h1>
+        </div>
       </div>
-      <div>
-        <p className="text-txt text-2xl px-10 max-w-screen-xl font-thin leading-loose tracking-widest	">
-          Hello! I'm Dhaniyel, a passionate beginner full-stack developer with a
-          year of experience in creating user-friendly and responsive web
-          applications. I am proficient in HTML, CSS, and JavaScript, and I
-          possess a strong understanding of front-end development fundamentals.
-          My goal is to craft visually appealing and interactive user
-          experiences that engage and inspire users. I thrive on learning new
-          technologies and continuously improving my skills to stay ahead in the
-          ever-evolving world of web development.
+      <div className="p-10">
+        {" "}
+        {/* Padding for the paragraph */}
+        <p className="text-lg text-center text-[#EEE9C7]">
+          {" "}
+          {/* Added text styling */}I am a software engineer with a passion for
+          creating user-friendly and accessible digital experiences. I have
+          worked on various projects, from small startups to large corporations,
+          and I am always looking for new opportunities to learn and grow.
         </p>
       </div>
     </div>
