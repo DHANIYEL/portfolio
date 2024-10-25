f01d2bedf9a090a9d86ba1d2c4283eb97d279b53
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-10-25 19:34:24 +0530

    make each icon bg big

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -5,21 +5,6 @@ import cssIcon from "../../public/icon/css.svg";
 import jsIcon from "../../public/icon/js.svg";
 import tailwindlIcon from "../../public/icon/tailwind.svg";
 import viteIcon from "../../public/icon/vite.svg";
-import reactIcon from "../../public/icon/react.svg";
-import tsIcon from "../../public/icon/ts.svg";
-import nextIcon from "../../public/icon/next.svg";
-import mongoIcon from "../../public/icon/mongo.svg";
-import nodeIcon from "../../public/icon/node.svg";
-import saasIcon from "../../public/icon/saas.svg";
-import figmaIcon from "../../public/icon/figma.svg";
-import wordpressIcon from "../../public/icon/wordpress.svg";
-import aeIcon from "../../public/icon/after-effects.svg";
-import framerIcon from "../../public/icon/framer-motion.svg";
-import gsapIcon from "../../public/icon/gsap-greensock.svg";
-import gitIcon from "../../public/icon/git.svg";
-import githubIcon from "../../public/icon/github.svg";
-import vscodeIcon from "../../public/icon/vscode.svg";
-import ppIcon from "../../public/icon/premiere-pro.svg";
 
 const page = () => {
   return (
@@ -30,63 +15,65 @@ const page = () => {
       {/* main div */}
       <div className="flex justify-center items-center">
         {/* grid layout 1st languages */}
-        <div className="">
-          {/*  js html */}
-          <div className="flex gap-5">
-            <div className="grid grid-cols-2 gap-3 w-full">
-              <div className="bg-[#0B1120] rounded-md overflow-hidden flex justify-center p-5">
-                <Image
-                  src={viteIcon}
-                  alt="img"
-                  layout="responsive"
-                  width={100}
-                  height={100}
-                  className="object-contain w-full h-full"
-                />
-              </div>
-              <div className="bg-[#2965F1] rounded-md overflow-hidden flex justify-center p-5">
-                <Image
-                  src={cssIcon}
-                  alt="img"
-                  layout="responsive"
-                  width={100}
-                  height={100}
-                  className="object-contain w-full h-full"
-                />
-              </div>
-              <div className="bg-[#F16529] rounded-md overflow-hidden flex justify-center p-5">
-                <Image
-                  src={htmlIcon}
-                  alt="img"
-                  layout="responsive"
-                  width={100}
-                  height={100}
-                  className="object-contain w-full h-full"
-                />
-              </div>
-              <div className="bg-[#0B1120] rounded-md overflow-hidden flex justify-center p-5">
-                <Image
-                  src={tailwindlIcon}
-                  alt="img"
-                  layout="responsive"
-                  width={100}
-                  height={100}
-                  className="object-contain w-full h-full"
-                />
-              </div>
+        <div className="flex gap-5 w-full max-w-screen-lg">
+          {/* grid section */}
+          <div className="grid grid-cols-2 gap-5 flex-1 w-full">
+            <div className="bg-[#0B1120] rounded-md overflow-hidden flex justify-center items-center p-6">
+              <Image
+                src={viteIcon}
+                alt="img"
+                layout="responsive"
+                width={100}
+                height={100}
+                className="object-contain w-full h-full"
+              />
             </div>
-
-            <div className="flex items-center justify-center bg-[#F7DF1E]  rounded-md p-5 overflow-hidden w-full h-full">
-              {/* js big icon */}
-              <Image src={jsIcon} alt="img" className="w-full h-full" />
+            <div className="bg-[#2965F1] rounded-md overflow-hidden flex justify-center items-center p-6">
+              <Image
+                src={cssIcon}
+                alt="img"
+                layout="responsive"
+                width={100}
+                height={100}
+                className="object-contain w-full h-full"
+              />
+            </div>
+            <div className="bg-[#F16529] rounded-md overflow-hidden flex justify-center items-center p-6">
+              <Image
+                src={htmlIcon}
+                alt="img"
+                layout="responsive"
+                width={100}
+                height={100}
+                className="object-contain w-full h-full"
+              />
+            </div>
+            <div className="bg-[#0B1120] rounded-md overflow-hidden flex justify-center items-center p-6">
+              <Image
+                src={tailwindlIcon}
+                alt="img"
+                layout="responsive"
+                width={100}
+                height={100}
+                className="object-contain w-full h-full"
+              />
             </div>
           </div>
 
-          {/* react ts next etc */}
-          <div></div>
+          {/* JS big icon, slightly larger */}
+          <div className="bg-[#F7DF1E] rounded-md overflow-hidden flex justify-center items-end flex-1 px-5">
+            <div className="w-3/4 h-3/4 flex justify-center items-center">
+              <Image
+                src={jsIcon}
+                alt="img"
+                layout="responsive"
+                width={100}
+                height={100}
+                className="object-contain"
+              />
+            </div>
+          </div>
         </div>
-        {/* grid layout 2nd tools */}
-        <div></div>
       </div>
     </div>
   );
